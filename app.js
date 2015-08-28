1f797b0b7bad2220b2ecbec41ed941cfd5186328
Ramezcua <ramezucua7@gmail.com>
2015-08-27 21:45:52 -0500

    Setup handlebars

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 var express = require('express');
+var exphbs = require('express-handlebars');
+
 var app = express();
 
+app.set('views', __dirname + '/project/views')
+
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main',
+  layoutsDir: __dirname + '/project/views/layouts'
+}));
+app.set('view engine', 'handlebars');
+
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.render('index');
 });
 
 var server = app.listen(3000, function () {
